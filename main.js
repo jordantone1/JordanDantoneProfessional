d2b69b3b7a001b0f1307acf02dba713394051dbc
jordantone1 <jordantone1@gmail.com>
2025-11-23 16:09:20 -0600

    Update main.js

--- main.js
+++ main.js
@@ -141,7 +141,7 @@ class PortfolioManager {
                 'Created a large multi-site network (3 sites) and configured DHCP for sites, IP configurations, and confirming all servers and routers can \'speak\' to one another.',
             technologies: ['Cisco Packet Tracer', 'Routers', 'Switches', 'IP Configuration', 'DHCP Site Configuration'],
             image: 'https://i.ibb.co/XxrZQZsN/Cisco-Lab-network1.png',
-            type: 'Infrastructure',
+            type: 'LAB - Infrastructure',
             status: 'Completed',
             date: '2024-04-10'
         }
